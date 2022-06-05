1bcfaca41e0955bb1455b33a36d9db482b0877a8
muslim0602 <muslimturaboyev0602@gmail.com>
2022-06-05 21:25:18 +0500

    Add toggle on dark-mode

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const elSiteThemeToggler = document.querySelector(".site-header-theme-toggler");
+
+elSiteThemeToggler.addEventListener("click" , function name(params) {
+  document.body.classList.toggle("dark-mode")
+
+});
